cf88b9cc8ce220f5100a78547df5e92778f8c8fe
Naseem Siddiqui <naseem.siddiqui5@gmail.com>
2011-09-04 18:00:34 +0500

    Skatax Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skatax::Application.config.secret_token = '58fcf6822b8993e1527deceb81d8918988c570e5fbf652e43d328ad9f57ad407f13827ae5cc6849ed4b223a5d97e7ee10635b6034062945373d8738d2d8f650b'
